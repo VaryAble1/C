a262da5336d1744b065c459d25fe216c8e920911
VeryAble <VaryAble@protonmail.ch>
2018-06-18 22:23:01 -0700

    Create mario2.c

---
+++ mario2.c
@@ -0,0 +1,14 @@
+// Mario Bro. question bank
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n = get_int("Number: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("?");
+    }
+    printf("\n");
+// Thanks for looking
+}
