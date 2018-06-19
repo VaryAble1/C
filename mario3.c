96fc5ede0e5a3708b9e1ce272d73c88c230883e1
VeryAble <VaryAble@protonmail.ch>
2018-06-18 22:35:49 -0700

    Create mario3.c

---
+++ mario3.c
@@ -0,0 +1,21 @@
+// Mario Bro. question bank
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt user for a positive number
+    int n;
+    do
+    {
+        n = get_int("Positive Number: ");
+    }
+    while (n <= 0);
+
+    // Print n number of bricks
+    for (int i = 0; i < n; i++)
+    {
+        printf("#\n");
+    }
+// Thanks for looking
+}
