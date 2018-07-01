d254d2ea33d58102c58646474fb3a17c535c6619
VaryAble <VaryAble@protonmail.ch>
2018-07-01 10:49:53 -0700

    Create linear.c

---
+++ linear.c
@@ -0,0 +1,36 @@
+// Linear search
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Names in a phone book of past instructors of CS50 Harvard
+string book[] = {
+    "Chen",
+    "Kernighan",
+    "Leitner",
+    "Lewis",
+    "Malan",
+    "Muller",
+    "Seltzer",
+    "Shieber",
+    "Smith"
+};
+
+int main(void)
+{
+    // Prompt user for name
+    string name = get_string("Name: ");
+
+    // Search for name
+    for (int i = 0; i < sizeof(book) / sizeof(string); i++)
+    {
+        if (strcmp(name, book[i]) == 0)
+        {
+            printf("Calling %s\n", name);
+            return 0;
+        }
+    }
+    printf("Quitting\n");
+// Thanks for looking
+}
